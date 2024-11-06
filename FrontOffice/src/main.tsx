056b0247123df116b85f5fca09c572ccb30e522d
inflames32 <cahuzac.p@gmail.com>
2024-11-06 15:35:11 +0100

    :fire: css to SCSS

--- FrontOffice/src/main.tsx
+++ FrontOffice/src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
 
-import "./styles/reset.css";
-import "./styles/index.css";
+import "./styles/reset.scss";
+import "./styles/index.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
