e52a823255c0c80661cea5266c38e0d3db5a2bfa
inflames32 <cahuzac.p@gmail.com>
2024-11-06 14:41:15 +0100

    :fire: create react router and layout

---
+++ FrontOffice/src/Pages/Homepage.tsx
@@ -0,0 +1,13 @@
+import Layout from "../Components/Layout";
+
+const Hompepage = () => {
+  return (
+    <div>
+      <Layout>      
+        <h1>Homepage</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default Hompepage;
