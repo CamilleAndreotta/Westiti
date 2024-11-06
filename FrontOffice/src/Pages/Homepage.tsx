93112c3c6e0503fe757427b3ef67be63332279ac
ZeSinitry <maximecorraza@yahoo.fr>
2024-11-06 17:16:09 +0100

    :fire: Update Homepage

--- FrontOffice/src/Pages/Homepage.tsx
+++ FrontOffice/src/Pages/Homepage.tsx
@@ -1,13 +1,27 @@
-import Layout from "../Components/Layout";
+import React from "react";
+import { Link } from "react-router-dom";
+import image from "../assets/img/loadingscreen.webp";
+import "../styles/homepage.css";
 
-const Hompepage = () => {
+const Homepage: React.FC = () => {
   return (
-    <div>
-      <Layout>      
-        <h1>Homepage</h1>
-      </Layout>
+    <div className="homepage">
+      <div className="left__section">
+        <h1>Bienvenue sur Westiti</h1>
+        <div className="buttons">
+          <Link to="/signup" className="signup__button">
+            Signup
+          </Link>
+          <Link to="/signin" className="signin__button">
+            Signin
+          </Link>
+        </div>
+      </div>
+      <div className="right__section">
+        <img src={image} alt="Home" />
+      </div>
     </div>
   );
 };
 
-export default Hompepage;
+export default Homepage;
