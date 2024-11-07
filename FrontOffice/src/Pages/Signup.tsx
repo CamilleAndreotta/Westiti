31b5ac73db778b0aa4570c346e5fba7b42a578f6
inflames32 <cahuzac.p@gmail.com>
2024-11-07 10:53:04 +0100

    :lipstick: improve css for signup

--- FrontOffice/src/Pages/Signup.tsx
+++ FrontOffice/src/Pages/Signup.tsx
@@ -3,95 +3,101 @@ import Layout from "../Components/Layout";
 
 import "../styles/signup.css";
 import Button from "../Components/Button";
+import Input from "../Components/Input";
 
 const Signup = () => {
-  //  const image = "../assets/img/logo.svg";
   const [signup, setSignup] = useState({
     email: "",
     password: "",
     password_confirmation: "",
   });
 
-  const inputChange = (e: any) => {
-    setSignup({ ...signup, [e.target.name]: e.target.value });
+  const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSignup((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
   };
 
-  const submit = async (e: any) => {
+  const submit = async (e: React.FormEvent) => {
     e.preventDefault();
+
     if (signup.password !== signup.password_confirmation) {
       console.log("Les mots de passe ne correspondent pas");
       return;
     }
+
     if (!signup.password || !signup.password_confirmation) {
       console.log("Pas de mots de passe rentré");
       return;
     }
+
     if (!signup.email) {
       console.log("Pas d'email");
       return;
     }
+
     try {
-      const response = await  fetch("http://localhost:3000/signup", {
+      const response = await fetch("http://localhost:3000/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(signup),
-      }); 
-     /*  fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))*/
+      });
       const data = await response.json();
-      console.log(data);
-      if (data.status !== 200) {
-        console.log("Une erreur c'est produite pednant la création du compte");
+
+      if (!response.ok) {
+        console.log("Une erreur s'est produite pendant la création du compte");
+        return;
       }
+
       console.log("Compte créé avec succès");
     } catch (error) {
-      console.log(error);
+      console.log("Erreur:", error);
     }
   };
+
   return (
     <Layout>
       <div className="signup">
         <div className="signup__container">
-          <form
-            action="submit"
-            className="signup__form"
-            onSubmit={(e) => submit(e)}
-          >
-            <input
+          <form action="submit" className="signup__form" onSubmit={submit}>
+            <Input
               type="text"
-              placeholder="email"
-              className="signup__input"
+              placeholder="Email"
               name="email"
               value={signup.email}
               onChange={inputChange}
-            />
-            <input
-              type="password"
-              placeholder="password"
               className="signup__input"
+            />
+            <Input
+              type="password"
+              placeholder="Mot de passe"
               name="password"
               value={signup.password}
               onChange={inputChange}
-            />
-            <input
-              type="password"
-              placeholder="password__confirmation"
               className="signup__input"
+            />
+            <Input
+              type="password"
+              placeholder="Confirmation du mot de passe"
               name="password_confirmation"
               value={signup.password_confirmation}
               onChange={inputChange}
+              className="signup__input"
             />
-            <Button disabled={
+            <Button
+              disabled={
                 !signup.email ||
                 !signup.password ||
                 !signup.password_confirmation
               }
-              type="button"
+              type="submit" // Utilisation de type="submit" pour le bouton du formulaire
               className="signup__submit"
-              onClick={(e: HTMLButtonElement) => submit(e)}> Création de compte</Button>
+            >
+              Création de compte
+            </Button>
           </form>
         </div>
       </div>
