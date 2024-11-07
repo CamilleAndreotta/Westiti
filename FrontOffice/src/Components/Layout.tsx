8f4760ead97becc6b9761cb951f6570ce33b2a81
inflames32 <cahuzac.p@gmail.com>
2024-11-07 10:53:04 +0100

    :lipstick: improve css for signup

--- FrontOffice/src/Components/Layout.tsx
+++ FrontOffice/src/Components/Layout.tsx
@@ -1,6 +1,9 @@
 import Footer from "./Footer";
 import Header from "./Header";
 
+
+import '../styles/layout.css'
+
 const Layout = ({ children } : any) => {
   return (
     <div className="layout">
