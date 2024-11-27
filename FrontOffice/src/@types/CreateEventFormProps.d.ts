d510587be988c75b1caa5cb97050166f594970a2
inflames32 <cahuzac.p@gmail.com>
2024-11-27 10:54:34 +0100

    :art: refactoring EventsPage

---
+++ FrontOffice/src/@types/CreateEventFormProps.d.ts
@@ -0,0 +1,9 @@
+export interface CreateEventFormProps {
+  name: string;
+  started_at: string;
+  ended_at: string;
+  address: string;
+  content: string;
+  picture: string;
+  creator_id: any;
+}
