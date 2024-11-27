16a1c1c14ef90498299cea8e25f8b2bea62cde6e
inflames32 <cahuzac.p@gmail.com>
2024-11-27 10:54:34 +0100

    :art: refactoring EventsPage

--- FrontOffice/src/Pages/EventsPage.tsx
+++ FrontOffice/src/Pages/EventsPage.tsx
@@ -12,23 +12,39 @@ import "../styles/button.css";
 import "../styles/input.css";
 import Modale from "../Components/Modale";
 import AutoCompleteInput from "../Components/AutoCompleteInput";
-import axios from "axios";
-import { log } from "node:console";
+
 import useToast from "../Hooks/useToast";
 
-interface Event {
-  id: string;
-  image: string;
-  title: string;
-  description: string;
-}
-const eventId = "1";
+import {
+  getAllEventsUser,
+  handleCreateEventChange,
+  handleCreateEventSubmit,
+  handleEventCodeChange,
+  handleJoinEventSubmit,
+} from "../Utils/event.functions";
+
+import { EventProps } from "../@types/EventProps";
+import { CreateEventFormProps } from "../@types/CreateEventFormProps";
+
 const EventsPage: React.FC = () => {
   const navigate = useNavigate();
-  const [eventsData, setEventsData] = useState<Event[]>([]);
+  const { onError, onSuccess } = useToast();
+
   const [isCreateEventModalOpen, setIsCreateEventModalOpen] = useState(false);
   const [isJoinEventModalOpen, setIsJoinEventModalOpen] = useState(false);
-  const { onError, onSuccess } = useToast();
+  const [eventCode, setEventCode] = useState<string>("");
+  const [eventsList, setEventsList] = useState<EventProps[] | []>([]);
+  const [createEventForm, setCreateEventForm] = useState<CreateEventFormProps>({
+    name: "",
+    started_at: "",
+    ended_at: "",
+    address: "",
+    content: "",
+    picture:
+      "https://images.unsplash.com/photo-1460978812857-470ed1c77af0?q=80&w=1895&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    creator_id: localStorage.getItem("userId"),
+  });
+
   // Récupération de l'utilisateur connecté
   const userId = localStorage.getItem("userId");
   const accessToken = localStorage.getItem("access_token");
@@ -40,47 +56,13 @@ const EventsPage: React.FC = () => {
     }
   }, [accessToken, userId, navigate]);
 
-  // États pour les formulaires
-  const [createEventForm, setCreateEventForm] = useState({
-    name: "",
-    started_at: "",
-    ended_at: "",
-    address: "",
-    content: "",
-    picture: "http//pivutre.jpreg",
-    creator_id: localStorage.getItem("userId"),
-  });
-
-  const [eventCode, setEventCode] = useState("");
-  const [eventsList, setEventsList] = useState();
   // Récupération des événements de l'utilisateur
   useEffect(() => {
-    // get all events by user ID
-    const fetchData = async () => {
+    // get all user events
+    const userId: string | null = localStorage.getItem("userId");
+    const fetchData = async (): Promise<void> => {
       try {
-        const response = await axios.get(`http://localhost:3000/api/event`, {
-          params: { userId },
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-        console.log(response.data);
-        setEventsList(response.data);
-        /* .then((response) => {
-          if (!response.ok) {
-            throw new Error("Erreur lors de la récupération des événements");
-          }
-          return response.json();
-        })
-        .then((data: Event[]) => {
-          setEventsData(data);
-        })
-        .catch((error) => {
-          console.error(
-            "Erreur lors de la récupération des événements :",
-            error
-          );
-        }); */
+        getAllEventsUser(userId, setEventsList);
       } catch (error) {
         console.log(error);
       }
@@ -88,144 +70,22 @@ const EventsPage: React.FC = () => {
     fetchData();
   }, [userId, accessToken]);
 
-  // Gestion des changements dans le formulaire de création d'événement
-  const handleCreateEventChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setCreateEventForm({
-      ...createEventForm,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleCreateEventSubmit = async (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
-    e.preventDefault();
-
-    try {
-      const response: any = await axios.post(
-        "http://localhost:3000/api/event",
-        createEventForm,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
-      console.log(response);
-      if (response.status !== 201) {
-        onError("Erreur lors de la création de l'événement");
-        throw new Error("Erreur lors de la création de l'événement");
-      }
-
-      onSuccess("Événement créé avec succès");
-
-      setIsCreateEventModalOpen(false);
-
-      // Mise à jour de la liste des événements
-      setEventsData([...eventsData, newEvent]);
-
-      // Fermeture de la modale et réinitialisation du formulaire
-      setCreateEventForm({
-        name: "",
-        started_at: "",
-        ended_at: "",
-        address: "",
-        content: "",
-      });
-    } catch (error) {
-      console.error("Erreur lors de la création de l'événement :", error);
-    }
-  };
-
-  // Gestion des changements dans le formulaire de rejoindre un événement
-  const handleEventCodeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEventCode(e.target.value);
-  };
-
-  const handleJoinEventSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch(`http://localhost:3000/api/event/join`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify({
-          code: eventCode,
-          userId: userId,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error("Erreur lors de la jonction à l'événement");
-      }
-
-      const joinedEvent = await response.json();
-
-      // Mise à jour de la liste des événements
-      setEventsData([...eventsData, joinedEvent]);
-
-      // Fermeture de la modale et réinitialisation du formulaire
-      setIsJoinEventModalOpen(false);
-      setEventCode("");
-    } catch (error) {
-      console.error("Erreur lors de la jonction à l'événement :", error);
-    }
-  };
-
   return (
     <Layout>
       <div className="events__page">
         <h1 className="events__title">Voici vos événements</h1>
         <div className="events__container">
-          {eventsData.map((event) => (
-            <div key={event.id} className="event__card">
-              <Card
-                dataImage={event.image}
-                header={event.title}
-              />
-            </div>
-          ))}
-          {/*  <Link to={`/event/${eventId}`}>
-            <div className="event__card">
-              <Card
-                dataImage="https://images.unsplash.com/photo-1460978812857-470ed1c77af0?q=80&w=1895&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                header="Evenement"
-                content="Mariage numéro 1"
-              />
-            </div>
-          </Link> */}
           {eventsList &&
-            eventsList.map(
-              (event: {
-                id: string;
-                name: string;
-                content: string;
-                address: string;
-                started_at: string;
-                ended_at: string;
-                access_code: string;
-                upload_status: true;
-                picture: string;
-                created_at: string;
-                updated_at: string;
-                userId: string;
-              }) => (
-                <Link to={`/event/${event.id}`}>
-                  <Card
-                    dataImage="https://images.unsplash.com/photo-1460978812857-470ed1c77af0?q=80&w=1895&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    header={event.name}
-                    content={event.content}
-                    key={event.id}
-                  />
-                </Link>
-              )
-            )}
+            eventsList.map((event: EventProps) => (
+              <Link to={`/event/${event.id}`}>
+                <Card
+                  dataImage={event.picture}
+                  header={event.name}
+                  content={event.content}
+                  key={event.id}
+                />
+              </Link>
+            ))}
         </div>
         <div className="events__buttons-container">
           <Button
@@ -245,14 +105,28 @@ const EventsPage: React.FC = () => {
           onClose={() => setIsCreateEventModalOpen(false)}
         >
           <h2 className="modale__title">Créer un événement</h2>
-          <form onSubmit={handleCreateEventSubmit}>
+          <form
+            onSubmit={(e) =>
+              handleCreateEventSubmit(
+                e,
+                createEventForm,
+                onError,
+                onSuccess,
+                setEventsList,
+                setCreateEventForm,
+                setIsCreateEventModalOpen
+              )
+            }
+          >
             {/* Nom de l'événement */}
             <Input
               type="text"
               name="name"
               label="Nom de l'événement"
               value={createEventForm.name}
-              onChange={handleCreateEventChange}
+              onChange={(e) =>
+                handleCreateEventChange(e, setCreateEventForm, createEventForm)
+              }
               required
             />
             {/* Date et heure de début */}
@@ -261,9 +135,10 @@ const EventsPage: React.FC = () => {
               name="started_at"
               label="Date et heure de début"
               value={createEventForm.started_at}
-              onChange={handleCreateEventChange}
+              onChange={(e) =>
+                handleCreateEventChange(e, setCreateEventForm, createEventForm)
+              }
               required
-              //labelPosition="above"
             />
             {/* Date et heure de fin */}
             <Input
@@ -271,11 +146,12 @@ const EventsPage: React.FC = () => {
               name="ended_at"
               label="Date et heure de fin"
               value={createEventForm.ended_at}
-              onChange={handleCreateEventChange}
+              onChange={(e) =>
+                handleCreateEventChange(e, setCreateEventForm, createEventForm)
+              }
               required
-              //   labelPosition="above"
             />
-            {/* Lieu de l'événement avec autocomplétion */}
+
             <AutoCompleteInput
               value={createEventForm.address}
               onChange={(value) =>
@@ -288,7 +164,13 @@ const EventsPage: React.FC = () => {
               <textarea
                 name="content"
                 value={createEventForm.content}
-                onChange={handleCreateEventChange}
+                onChange={(e) =>
+                  handleCreateEventChange(
+                    e,
+                    setCreateEventForm,
+                    createEventForm
+                  )
+                }
                 required
               ></textarea>
               <label>Description de l'événement</label>
@@ -305,13 +187,17 @@ const EventsPage: React.FC = () => {
           onClose={() => setIsJoinEventModalOpen(false)}
         >
           <h2 className="modale__title">Rejoindre un événement</h2>
-          <form onSubmit={handleJoinEventSubmit}>
+          <form
+            onSubmit={(e) =>
+              handleJoinEventSubmit(e, setIsJoinEventModalOpen, setEventCode)
+            }
+          >
             <Input
               type="text"
               name="eventCode"
               label="Code de l'événement"
               value={eventCode}
-              onChange={handleEventCodeChange}
+              onChange={(e) => handleEventCodeChange(e, setEventCode)}
               required
             />
             <Button type="submit" className="btn modale__btn">
