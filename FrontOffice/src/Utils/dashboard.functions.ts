85b610d57773814fe20b495312448e0dd829ba4c
pierrecahuzac <cahuzac.p@gmail.com>
2024-12-16 14:30:45 +0100

    :recycle: refacto functions + adding traeffik in dockercompose

--- FrontOffice/src/Utils/dashboard.functions.ts
+++ FrontOffice/src/Utils/dashboard.functions.ts
@@ -1,73 +1,27 @@
-import axios from "axios";
 import { Dispatch, SetStateAction } from "react";
-import { EventProps } from "../@types/EventProps";
 import { CreateEventFormProps } from "../@types/CreateEventFormProps";
+import { axiosInstance } from "./axiosInstance";
 
-export const getAllEventsUser = async (
-  userId: string | null,
-  setEventsList: Dispatch<SetStateAction<EventProps[] | null>>
-) => {
-  const accessToken :string | null = localStorage.getItem("access_token");
-  const response = await axios.get(`http://localhost:3000/api/event`, {
+export const getAllEventsUser = async (userId: string | null) => {
+  const response = await axiosInstance.get(`event`, {
     params: { userId },
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  console.log(response);
-  setEventsList(response.data);
-  return;
+ });
+
+  return response.data;
 };
 
 export const handleCreateEventSubmit = async (
   e: React.FormEvent<HTMLFormElement>,
-  createEventForm: CreateEventFormProps,
-  onError: (error: string) => void,
-  onSuccess: (message: string) => void,
-  setEventsList: Dispatch<SetStateAction<EventProps[] | null>>,
-  setCreateEventForm: Dispatch<SetStateAction<CreateEventFormProps>>,
-  setIsCreateEventModalOpen: Dispatch<SetStateAction<boolean>>
-): Promise<void> => {
+  createEventForm: CreateEventFormProps
+): Promise<any> => {
   e.preventDefault();
-
-  const accessToken = localStorage.getItem("access_token");
-  const userId = localStorage.getItem("user_id");
-
   try {
-    const response: any = await axios.post(
-      "http://localhost:3000/api/event",
-      createEventForm,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
+    e.preventDefault();
+    const createdEvent: any = await axiosInstance.post(
+      "/event",
+      createEventForm
     );
-    setEventsList(null);
-    console.log(response);
-    if (response.status !== 201) {
-      onError("Erreur lors de la création de l'événement");
-      throw new Error("Erreur lors de la création de l'événement");
-    }
-
-    onSuccess("Événement créé avec succès");
-
-    // Mise à jour de la liste des événements
-    await getAllEventsUser(userId, setEventsList);
-
-    setIsCreateEventModalOpen(false);
-
-    // Fermeture de la modale et réinitialisation du formulaire
-    setCreateEventForm({
-      name: "",
-      started_at: "",
-      ended_at: "",
-      address: "",
-      content: "",
-      creator_id: localStorage.getItem("userId"),
-      picture: "http//pivutre.jpreg",
-    });
+    return { createdEvent };
   } catch (error) {
     console.error("Erreur lors de la création de l'événement :", error);
   }
@@ -89,46 +43,22 @@ export const handleCreateEventChange = (
 
 export const handleJoinEventSubmit = async (
   e: React.FormEvent<HTMLFormElement>,
-  setIsJoinEventModalOpen: Dispatch<SetStateAction<boolean>>,
-  setEventCode: Dispatch<SetStateAction<string | null>>,
   eventCode: string | null,
-  onSuccess: (message: string) => void,
-  onError: (message: string) => void
 ) => {
   e.preventDefault();
 
   try {
     const userId: string | null = localStorage.getItem("userId");
-    const data: {
-      userId: string | null;
-      access_code: string | null;
-    } = {
+    const data = {
       userId,
       access_code: eventCode,
-    };
-    const accessToken: string | null = localStorage.getItem("access_token");
-    const response = await axios.post(
-      `http://localhost:3000/api/event/join`,
+    };    
+    const response = await axiosInstance.post(
+      `/event/join`,
       data,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
     );
-
-    if (response.status !== 201) {
-      onError("Erreur lors de la jonction à l'événement");
-      throw new Error("Erreur lors de la jonction à l'événement");
-    }
-
-    // Fermeture de la modale et réinitialisation du formulaire
-    setIsJoinEventModalOpen(false);
-    setEventCode("");
-    onSuccess("Vous êtes maintenant membre de cet événement");
+    return response    
   } catch (error) {
-    onError("Erreur lors de la jonction à l'événement");
     console.error("Erreur lors de la jonction à l'événement");
   }
 };
