0fc8310f83081f0b9b09f05572ca3567131ff9e1
alexandremirou <alexandre.mirou@gmail.com>
2024-12-17 16:56:46 +0100

    :sparkles: add function to delete photos on an event & to delete an event by his creator

--- Api/src/photo/photo.controller.ts
+++ Api/src/photo/photo.controller.ts
@@ -3,17 +3,25 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { PhotoService } from './photo.service';
 import { CreatePhotoDto } from './dto/create-photo.dto';
-import { UpdatePhotoDto } from './dto/update-photo.dto';
+import { DeletePhotoDto } from './dto/delete-photo.dto';
+import { UserService } from 'src/user/user.service';
+import { EventService } from 'src/event/event.service';
+import { MulterService } from 'src/multer/multer.service';
 
-@Controller('photo')
+@Controller('/api/photo')
 export class PhotoController {
-  constructor(private readonly photoService: PhotoService) {}
+  constructor(
+    private readonly photoService: PhotoService,
+    private readonly userService: UserService,
+    private readonly eventService: EventService,
+    private readonly multerService: MulterService,
+  ) {}
 
   @Post()
   create(@Body() createPhotoDto: CreatePhotoDto) {
@@ -27,16 +35,29 @@ export class PhotoController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.photoService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePhotoDto: UpdatePhotoDto) {
-    return this.photoService.update(+id, updatePhotoDto);
+    return this.photoService.findOne(id);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.photoService.remove(+id);
+  public async remove(
+    @Param('id') photoId: string,
+    @Body() deletePhotoDto: DeletePhotoDto,
+  ) {
+    const user = await this.userService.findOne(deletePhotoDto.userId);
+    const event = await this.eventService.findOne(deletePhotoDto.eventId);
+    const photo = await this.photoService.findOne(photoId);
+
+    if (!(event.creator_id.id === user.id || photo.userId === user.id)) {
+      throw new UnauthorizedException({
+        message: "Vous n'avez pas les droits pour supprimer cette photo",
+      });
+    }
+
+    const files = [];
+    files.push(photo);
+
+    this.multerService.deleteFiles(files);
+
+    return this.photoService.remove(photoId);
   }
 }
--- Api/src/photo/photo.module.ts
+++ Api/src/photo/photo.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { PhotoService } from './photo.service';
 import { PhotoController } from './photo.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from 'src/user/user.service';
+import { EventService } from 'src/event/event.service';
+import { MulterService } from 'src/multer/multer.service';
 
 @Module({
   controllers: [PhotoController],
-  providers: [PhotoService, PrismaService],
+  providers: [
+    PhotoService,
+    PrismaService,
+    UserService,
+    EventService,
+    MulterService,
+  ],
 })
 export class PhotoModule {}
